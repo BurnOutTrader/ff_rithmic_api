4550ec2b36d69282d0e0a4182a32f5e3e7887358
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-09-14 11:49:38 +1000

    update rdme

--- src/test.rs
+++ src/test.rs
@@ -138,15 +138,16 @@ pub async fn handle_received_responses(
                             println!("{:?}", pong)
                         }
                         Message::Close(close) => {
-                            //This message is sent on weekends, you can use this message to schedule a reconnection attempt for market open.
-                            /* Example of received market closed message
-                                received: Ok(Close(Some(CloseFrame { code: Normal, reason: "normal closure" })))
-                                Some(CloseFrame { code: Normal, reason: "normal closure" })
-                                Error: ServerErrorDebug("Failed to send RithmicMessage, possible disconnect, try reconnecting to plant TickerPlant: Trying to work with closed connection")
-                            */
+                            // receive this message when market is closed.
+                            // received: Ok(Close(Some(CloseFrame { code: Normal, reason: "normal closure" })))
                             println!("{:?}", close)
                         }
                         Message::Frame(frame) => {
+                            //This message is sent on weekends, you can use this message to schedule a reconnection attempt for market open.
+                            /* Example of received market closed message
+                                Some(CloseFrame { code: Normal, reason: "normal closure" })
+                                Error: ServerErrorDebug("Failed to send RithmicMessage, possible disconnect, try reconnecting to plant TickerPlant: Trying to work with closed connection")
+                            */
                             println!("{}", frame)
                         }
                     }
